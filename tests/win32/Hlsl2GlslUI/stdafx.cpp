9e0a1dbbd6cdb3bd462b4b1f125f87ce9d04d464
Aras Pranckevicius <aras@unity3d.com>
2010-02-24 09:55:06 +0200

    remove generated docs, standalone project file, UI source

--- tests/win32/Hlsl2GlslUI/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Hlsl2GlslUI.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
