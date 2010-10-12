1f09e502f06cdb68eda055510809781bbba27e1a
Aras Pranckevicius <aras@unity3d.com>
2010-10-12 17:03:36 +0300

    unbreak windows build

--- hlslang/OSDependent/Windows/main.cpp
+++ hlslang/OSDependent/Windows/main.cpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE.txt file.
 
-
+#include "osinclude.h"
 #include "InitializeDll.h"
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
