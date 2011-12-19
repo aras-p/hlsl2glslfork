6e166befbbeade63c62b1c606859aea4df30b1fd
Glen Miner <shaggie76@hotmail.com>
2011-12-19 17:55:51 -0500

    Cherry picked '1c64e1d91ee970eb20e2219e7442f393986acc5a' from https://github.com/shaggie76/hlsl2glslfork.git

--- hlslang/GLSLCodeGen/glslOutput.cpp
+++ hlslang/GLSLCodeGen/glslOutput.cpp
@@ -82,7 +82,7 @@ void writeConstantConstructor( std::stringstream& out, EGlslSymbolType t, TPreci
             out << c->getIConst();
             break;
          case EbtFloat:
-            out << c->getFConst();
+            GlslSymbol::writeFloat(out, c->getFConst());
             break;
          default:
             assert(0);
