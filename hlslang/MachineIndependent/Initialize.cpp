b60cb9255cff700305803ec6a8ced8cffab3c8cc
Jim Sagevid <jim.sagevid@bitsquid.se>
2012-04-10 09:07:56 +0200

    Missed shadow2DProj support.

--- hlslang/MachineIndependent/Initialize.cpp
+++ hlslang/MachineIndependent/Initialize.cpp
@@ -406,6 +406,7 @@ void TBuiltIns::initialize()
       s.append(TString("float4 tex1D(sampler1DShadow s, float2 coord);"));
       s.append(TString("float4 tex1D(sampler1D s, float coord, float ddx, float ddy);"));
       s.append(TString("float4 tex1Dproj(sampler1D s, float4 coord);"));
+      s.append(TString("float4 tex1Dproj(sampler1DShadow s, float4 coord);"));
       s.append(TString("float4 tex1Dbias(sampler1D s, float4 coord);"));
       s.append(TString("float4 tex1Dlod(sampler1D s, float4 coord);"));
       s.append(TString("float4 tex1Dgrad(sampler1D s, float coord, float ddx, float ddy);"));
@@ -414,6 +415,7 @@ void TBuiltIns::initialize()
       s.append(TString("float4 tex2D(sampler2DShadow s, float3 coord);"));
       s.append(TString("float4 tex2D(sampler2D s, float2 coord, float2 ddx, float2 ddy);"));
       s.append(TString("float4 tex2Dproj(sampler2D s, float4 coord);"));
+	  s.append(TString("float4 tex2Dproj(sampler2DShadow s, float4 coord);"));
       s.append(TString("float4 tex2Dbias(sampler2D s, float4 coord);"));
       s.append(TString("float4 tex2Dlod(sampler2D s, float4 coord);"));
       s.append(TString("float4 tex2Dgrad(sampler2D s, float2 coord2, float2 ddx, float2 ddy);"));
@@ -435,6 +437,7 @@ void TBuiltIns::initialize()
       s.append(TString("float4 texRECT(samplerRECT s, float2 coord);"));
       s.append(TString("float4 texRECT(samplerRECTShadow s, float3 coord);"));
       s.append(TString("float4 texRECTproj(samplerRECT s, float4 coord);"));
+      s.append(TString("float4 texRECTproj(samplerRECTShadow s, float4 coord);"));
       s.append(TString("float4 texRECTproj(samplerRECT s, float3 coord);"));
 	   
       s.append(TString("float4 tex1D(sampler s, float coord);"));
