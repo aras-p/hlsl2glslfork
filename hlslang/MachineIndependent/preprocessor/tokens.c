df3c5e5f89f499384bb2726eae1b291e821adf0a
Aras Pranckevicius <aras@unity3d.com>
2013-04-21 09:37:53 +0300

    fix warning

--- hlslang/MachineIndependent/preprocessor/tokens.c
+++ hlslang/MachineIndependent/preprocessor/tokens.c
@@ -182,7 +182,7 @@ void RecordToken(TokenStream *pTok, int token, yystypepp * yylvalpp)
          str=yylvalpp->symbol_name;
          while (*str){
             lAddByte(pTok,(unsigned char) *str);
-            *str++;
+            str++;
          }
          lAddByte(pTok, 0);
          break;
