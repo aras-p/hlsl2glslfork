1d6290f4df8e4c3f05ab4b943579cc2dee3a353c
Ray Donnelly <mingw.android@gmail.com>
2013-09-05 02:24:28 +0100

    warnings: fix breaks strict aliasing
    Remove isinff(x) replacing it with !isfinite(x)
    (or _finite on Windows MSVC < 2013)

--- hlslang/MachineIndependent/preprocessor/scanner.c
+++ hlslang/MachineIndependent/preprocessor/scanner.c
@@ -9,9 +9,12 @@
 
 #include "slglobals.h"
 
-#define isinff(x) (((*(int *)&(x) & 0x7f800000L)==0x7f800000L) && ((*(int *)&(x) & 0x007fffffL)==0000000000L))
-
-
+#if defined(_MSC_VER) && (_MSC_VER < 1800)
+# include <float.h>
+# define isfinite(_d) _finite(_d)
+#else
+# include <math.h>
+#endif
 
 typedef struct StringInputSrc {
     InputSrc base;
@@ -113,7 +116,6 @@ static float lBuildFloatValue(const char *str, int len, int exp)
 {
     double val, expval, ten;
     int ii, absexp;
-    float rv;
 
     val = 0.0;
     for (ii = 0; ii < len; ii++)
@@ -134,11 +136,10 @@ static float lBuildFloatValue(const char *str, int len, int exp)
             val /= expval;
         }
     }
-    rv = (float)val;
-    if (isinff(rv)) {
+    if (!isfinite(val)) {
 		CPPErrorToInfoLog(" ERROR___FP_CONST_OVERFLOW");
     }
-    return rv;
+    return (float)val;
 } // lBuildFloatValue
 
 
