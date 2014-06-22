e2fb46c0058e62d6658521ab550afb73730c43e5
Aras Pranckevicius <aras@unity3d.com>
2014-06-22 12:50:38 +0300

    preprocessor: remove old preprocessor code, will replace with Mojoshader soon

--- hlslang/MachineIndependent/preprocessor/preprocess.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) The HLSL2GLSLFork Project Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE.txt file.
-
-# include "slglobals.h"
-extern CPPStruct *cpp;
-int ScanFromString(char *s);
